25232a6fdcb88bef4a395db4653b6c9fdcabe301
Dario Coletto <darocoletto@gmail.com>
2025-12-01 17:17:43 -0300

    01-BruteForce / implement rate limiting and brute force protection in authentication routes

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const {
+
+  limiter,
+ speedLimiter
+} = require('../middleware/loginSecurity');
 
 // Rutas de autenticación
-router.post('/login', authController.login);
+router.post('/login', speedLimiter, limiter , authController.login);
 router.post('/register', authController.register);
 router.post('/auth/verify', authController.verifyToken);
 router.post('/check-username', authController.checkUsername);
